472b094e93d41b7f12a2aa3c3662904a8d879c30
mpls104 <31480737+mpls104@users.noreply.github.com>
2020-08-30 21:57:35 +0900

    Change important infos

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `kohaluka`,
+        short_name: `kohaluka`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
